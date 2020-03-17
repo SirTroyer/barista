6b2e0a6afb46c94e22af567829df3d74a37196b4
Thomas Heller <thomas.heller@dynatrace.com>
2020-03-17 08:15:28 +0100

    chore(button): Added custom-elements representation for a button.
    Fixes #729

--- apps/dev/src/button/button-demo.component.ts
+++ apps/dev/src/button/button-demo.component.ts
@@ -15,10 +15,28 @@
  */
 
 import { Component } from '@angular/core';
+import '@dynatrace/fluid-elements/button';
+import {
+  ButtonVariant,
+  DtButtonThemePalette,
+} from '@dynatrace/barista-components/button';
 
 @Component({
   selector: 'button-dev-app-demo',
   templateUrl: './button-demo.component.html',
   styleUrls: ['./button-demo.component.scss'],
 })
-export class ButtonDemo {}
+export class ButtonDemo {
+  disabled: boolean = false;
+
+  variant: ButtonVariant = 'primary';
+
+  color: DtButtonThemePalette = 'main';
+
+  events: any[] = [];
+
+  clickHandler(event: MouseEvent): void {
+    this.events.push(event);
+    console.log(event.target);
+  }
+}
